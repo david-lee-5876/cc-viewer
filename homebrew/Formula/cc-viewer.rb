cc69885748e4f92dd3b50a8cc0a621d70f478153
weiesky.wangc <weiesky.wangc@antgroup.com>
2026-06-30 11:05:16 +0800

    chore(homebrew): bump formula to 1.6.330

--- homebrew/Formula/cc-viewer.rb
+++ homebrew/Formula/cc-viewer.rb
@@ -4,8 +4,8 @@ class CcViewer < Formula
   desc "Vibe Coding toolkit for Claude Code with Web Viewer + Logger"
   homepage "https://github.com/weiesky/cc-viewer"
   # NOTE: url + sha256 自动由 .github/workflows/bump-homebrew.yml 维护，发版后会跨 repo PR 更新
-  url "https://registry.npmjs.org/cc-viewer/-/cc-viewer-1.6.224.tgz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  url "https://registry.npmjs.org/cc-viewer/-/cc-viewer-1.6.330.tgz"
+  sha256 "157118f463e683026b90cb46cb4f9aecd38c99d479d4aa8284c08cb75ce2654c"
   license "MIT"
 
   depends_on "node"
